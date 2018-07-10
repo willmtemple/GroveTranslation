569101050741a943a100aaa22a0fe907572f46e0
Tanner Quigley <TannerQuigley@me.com>
2018-07-10 10:57:38 -0600

    init commit

---
+++ translation.js
@@ -0,0 +1,60 @@
+"use strict";
+exports.__esModule = true;
+var GrovePi = require("./libs");
+var Commands = GrovePi.commands;
+var Board = GrovePi.board;
+var AccelerationI2cSensor = GrovePi.sensors.AccelerationI2C;
+var UltrasonicDigitalSensor = GrovePi.sensors.UltrasonicDigital;
+var AirQualityAnalogSensor = GrovePi.sensors.AirQualityAnalog;
+var DHTDigitalSensor = GrovePi.sensors.DHTDigital;
+var LightAnalogSensor = GrovePi.sensors.LightAnalog;
+var DigitalButtonSensor = GrovePi.sensors.DigitalButton;
+var LoudnessAnalogSensor = GrovePi.sensors.LoudnessAnalog;
+var RotaryAngleAnalogSensor = GrovePi.sensors.RotaryAnalog;
+var DustDigitalSensor = GrovePi.sensors.dustDigital;
+var DigitalOutput = GrovePi.sensors.DigitalOutput;
+var grove;
+(function (grove) {
+    // Led
+    function ledOn(port) {
+        var led = new DigitalOutput(port);
+        led.turnOn();
+    }
+    function ledOff(port) {
+        var led = new DigitalOutput(port);
+        led.turnOff();
+    }
+    // Ultrasonic Ranger
+    function pollUltrasonicRanger(port) {
+        var ultrasonicSensor = new UltrasonicDigitalSensor(port);
+        ultrasonicSensor.on('change', function (res) {
+            // Do on Change
+        });
+        ultrasonicSensor.watch();
+    }
+    function pollButtonPress(port) {
+        var buttonSensor = new DigitalButtonSensor(port);
+        buttonSensor.on('down', function (res) {
+            if (res == 'longpress') {
+                // Handle long press
+            }
+            else {
+                // handle short press
+            }
+        });
+        buttonSensor.watch();
+    }
+    function pollRotaryAngle(port) {
+        var rotaryAngleSensor = new RotaryAngleAnalogSensor(port);
+        rotaryAngleSensor.start();
+        rotaryAngleSensor.on('data', function (res) {
+            // Do on Change
+        });
+    }
+    function getRotaryAngleValue(port) {
+        var rotaryAngleSensor = new RotaryAngleAnalogSensor(port);
+        rotaryAngleSensor.start();
+        return rotaryAngleSensor.read();
+    }
+})(grove || (grove = {}));
+exports["default"] = grove;
